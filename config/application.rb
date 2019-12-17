369d29eaf121ff2cc8c0dfc83e544748ff5dafad
Manasa Rao <drmanasa18.rao@gmail.com>
2019-12-17 11:38:59 +0100

    adds updated coveralls

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module ExtremeInvoicingOctober2019
       generate.helper_specs false
       generate.routing_specs false
       generate.controller_specs false
+      generate.system_tests = false
     end
   end
 end
